491e8cba150418649f5659267786fccde30f4bf2
happyanna05 <120617402+happyanna05@users.noreply.github.com>
2025-08-08 11:04:13 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -18,4 +18,12 @@ $(function () {
     $('html, body').animate({ scrollTop: 0 }, 400);
     return false;
   });
+
+    let mouseCursor = document.getElementById("cursor");//지정된 ID와 일치하거나 일치하는 요소를 문서에서 발견해서 반환
+    //let: 변수 재할당 해야 하는 경우에 그게 아니라면 대부분 const
+    document.addEventListener("mousemove", cursor);
+    function cursor(e) {
+      mouseCursor.style.top = e.pageY + document.body.scrollTop + "px"; //스크롤시에 페이지의 상단의 위치값을 반환하거나 부여한다.
+      mouseCursor.style.left = e.pageX + document.body.scrollLeft + "px";//스크롤시에 페이지의 좌측의 위치값을 반환하거나 부여한다.
+    }
 })
